538d5b28e6d83c563117ed478779c915266322f8
Hongjo Lim <czebahi@gmail.com>
2020-09-26 17:54:52 -0400

    Changed job model properties

--- jobs/models.py
+++ jobs/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200, blank=False)
+    title = models.CharField(max_length=200, blank=False)
+    company = models.CharField(max_length=200, blank=False)
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path('', jobs.views.home, name = 'home'),
     path('home', jobs.views.home, name = 'home'),
 
-] + static(settings.STATIC_URL, documnet_root=settings.STATIC_ROOT)
+] 
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
