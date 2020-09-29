82e89de36b1825b83051ab65d6c170f49821b78d
Hongjo Lim <czebahi@gmail.com>
2020-09-29 19:52:07 -0400

    Changed Job Model

--- jobs/models.py
+++ jobs/models.py
@@ -4,6 +4,8 @@ class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     title = models.CharField(max_length=200, blank=False)
     company = models.CharField(max_length=200, blank=False)
+    startdate = models.DateField(blank = False)
+    enddate = models.DateField(blank = False)
     detail = models.TextField(max_length=1000, blank=False , default="")
 
     def __str__(self):
